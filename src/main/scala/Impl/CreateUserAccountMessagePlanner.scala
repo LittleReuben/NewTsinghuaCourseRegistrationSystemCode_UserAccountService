d71f870cb6c272beb8f56380b8121afbadc3b4de
LRYP <2213469156@qq.com>
2025-07-01 23:36:01 +0800

    自动提交：更新 CreateUserAccountMessagePlanner (APIPlan) 的代码

--- src/main/scala/Impl/CreateUserAccountMessagePlanner.scala
+++ src/main/scala/Impl/CreateUserAccountMessagePlanner.scala
@@ -1,25 +1,28 @@
 package Impl
 
 
+import Common.API.{PlanContext, Planner}
+import Common.DBAPI._
+import Common.Object.ParameterList
+import Common.Object.SqlParameter
+import Common.ServiceUtils.schemaName
 import Objects.UserAccountService.UserInfo
+import Objects.UserAccountService.SafeUserInfo
 import Objects.UserAccountService.UserRole
-import Utils.UserAccountProcess.fetchSafeUserInfoByID
 import Utils.UserAccountProcess.validateAdminToken
-import Utils.UserAccountProcess.recordUserAccountOperationLog
 import Utils.UserAccountProcess.validateAccountNameUniqueness
-import Objects.SystemLogService.SystemLogEntry
-import Common.API.{PlanContext, Planner}
-import Common.DBAPI._
-import Common.Object.SqlParameter
-import Common.ServiceUtils.schemaName
+import Utils.UserAccountProcess.recordUserAccountOperationLog
 import org.slf4j.LoggerFactory
-import org.joda.time.DateTime
 import cats.effect.IO
-import cats.implicits._
 import io.circe._
 import io.circe.syntax._
 import io.circe.generic.auto._
+import cats.implicits._
+import org.joda.time.DateTime
+import Utils.UserAccountProcess.fetchSafeUserInfoByID
 import Common.Serialize.CustomColumnTypes.{decodeDateTime, encodeDateTime}
+import Objects.SystemLogService.SystemLogEntry
+import Utils.UserAccountProcess.fetchUserInfoByToken
 import io.circe._
 import io.circe.syntax._
 import io.circe.generic.auto._
@@ -31,11 +34,9 @@ import cats.effect.IO
 import Common.Object.SqlParameter
 import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
 import Common.ServiceUtils.schemaName
-import Utils.UserAccountProcess.fetchUserInfoByToken
 import Objects.UserAccountService.SafeUserInfo
 import cats.implicits.*
 import Common.Serialize.CustomColumnTypes.{decodeDateTime,encodeDateTime}
-import Objects.UserAccountService.SafeUserInfo
 
 case class CreateUserAccountMessagePlanner(
     adminToken: String,
@@ -45,68 +46,86 @@ case class CreateUserAccountMessagePlanner(
     role: UserRole,
     override val planContext: PlanContext
 ) extends Planner[UserInfo] {
-
-  private val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
+  val logger = LoggerFactory.getLogger(this.getClass.getSimpleName + "_" + planContext.traceID.id)
 
   override def plan(using PlanContext): IO[UserInfo] = {
     for {
-      // Step 1: Validate adminToken
-      _ <- IO(logger.info(s"开始验证管理员 Token：${adminToken}"))
-      isAdminValid <- validateAdminToken(adminToken)
-      _ <- if (!isAdminValid) IO.raiseError(new IllegalArgumentException("权限不足")) else IO.unit
-      _ <- IO(logger.info("管理员 Token 验证通过"))
+      // Step 1: Validate admin token
+      _ <- IO(logger.info(s"[Step 1] Validating adminToken: ${adminToken}"))
+      _ <- validateAdminToken(adminToken).flatMap(isAdmin =>
+        if (!isAdmin) IO.raiseError(new Exception("权限不足")) else IO.unit
+      )
 
-      // Step 2: Validate accountName uniqueness
-      _ <- IO(logger.info(s"开始验证账号名是否唯一：${accountName}"))
-      isAccountUnique <- validateAccountNameUniqueness(accountName)
-      _ <- if (!isAccountUnique) IO.raiseError(new IllegalArgumentException("账号名重复")) else IO.unit
-      _ <- IO(logger.info("账号名验证通过"))
+      // Step 2: Validate account name uniqueness
+      _ <- IO(logger.info(s"[Step 2] Checking accountName uniqueness: ${accountName}"))
+      _ <- validateAccountNameUniqueness(accountName).flatMap(isUnique =>
+        if (!isUnique) IO.raiseError(new Exception("账号名重复")) else IO.unit
+      )
 
-      // Step 3: Create user in UserAccountTable
-      _ <- IO(logger.info(s"开始创建用户账号，Name: ${name}, AccountName: ${accountName}, Role: ${role.toString}"))
-      userID <- createUserAccount()
-      _ <- IO(logger.info(s"成功创建用户账号，生成的用户 ID: ${userID}"))
+      // Step 3: Insert new user into UserAccountTable and get new userID
+      _ <- IO(logger.info(s"[Step 3] Inserting new user: name=${name}, accountName=${accountName}, role=${role.toString}"))
+      userID <- insertUserRecord(name, accountName, password, role)
 
       // Step 4: Record operation log
-      _ <- IO(logger.info(s"开始记录操作日志"))
-      logMessage = s"创建用户账号，Name: ${name}, AccountName: ${accountName}, Password: [已加密], Role: ${role.toString}"
-      _ <- recordUserAccountOperationLog("增加", userID, logMessage)
-      _ <- IO(logger.info("操作日志记录成功"))
+      _ <- IO(logger.info(s"[Step 4] Recording operation log for created userID: ${userID}"))
+      _ <- recordUserAccountOperationLog("增加", userID, s"创建用户: name=${name}, accountName=${accountName}, role=${role.toString}")
 
-      // Step 5: Construct UserInfo object
-      _ <- IO(logger.info("开始构造返回的用户信息对象"))
-      userInfo <- IO {
-        UserInfo(userID, name, accountName, password, role)
-      }
-      _ <- IO(logger.info(s"成功构造返回的用户信息对象：${userInfo}"))
-
+      // Step 5: Construct and return UserInfo object
+      _ <- IO(logger.info(s"[Step 5] Constructing UserInfo object for userID: ${userID}"))
+      userInfo <- constructUserInfo(userID)
     } yield userInfo
   }
 
-  private def createUserAccount(using PlanContext): IO[Int] = {
-    for {
-      // SQL构造和参数准备
-      sql <- IO {
-        s"""
-         INSERT INTO ${schemaName}.user_account_table
-         (user_name, account_name, password, role)
+  private def insertUserRecord(
+      name: String,
+      accountName: String,
+      password: String,
+      role: UserRole
+  )(using PlanContext): IO[Int] = {
+    val sql =
+      s"""
+         INSERT INTO ${schemaName}.user_account_table (user_name, account_name, password, role)
          VALUES (?, ?, ?, ?)
          RETURNING user_id;
        """
-      }
-      parameters <- IO {
-        List(
-          SqlParameter("String", name),
-          SqlParameter("String", accountName),
-          SqlParameter("String", password),  // 密码这个字段应已加密，假设它已经由其他逻辑加密后传入
-          SqlParameter("String", role.toString)
+    val parameters = List(
+      SqlParameter("String", name),
+      SqlParameter("String", accountName),
+      SqlParameter("String", password),
+      SqlParameter("String", role.toString)
+    )
+
+    for {
+      _ <- IO(logger.info(s"[InsertUserRecord] SQL: $sql"))
+      _ <- IO(logger.info(s"[InsertUserRecord] Parameters: ${parameters.map(_.value).mkString(", ")}"))
+      userID <- readDBInt(sql, parameters)
+      _ <- IO(logger.info(s"[InsertUserRecord] Inserted userID: $userID"))
+    } yield userID
+  }
+
+  private def constructUserInfo(userID: Int)(using PlanContext): IO[UserInfo] = {
+    val sql =
+      s"""
+         SELECT user_id, user_name, account_name, password, role
+         FROM ${schemaName}.user_account_table
+         WHERE user_id = ?;
+       """
+    val parameters = List(SqlParameter("Int", userID.toString))
+
+    for {
+      _ <- IO(logger.info(s"[ConstructUserInfo] SQL: $sql"))
+      _ <- IO(logger.info(s"[ConstructUserInfo] Parameters: ${parameters.map(_.value).mkString(", ")}"))
+      json <- readDBJson(sql, parameters)
+      userInfo <- IO {
+        UserInfo(
+          userID = decodeField[Int](json, "user_id"),
+          userName = decodeField[String](json, "user_name"),
+          accountName = decodeField[String](json, "account_name"),
+          password = decodeField[String](json, "password"),
+          role = UserRole.fromString(decodeField[String](json, "role"))
         )
       }
-
-      _ <- IO(logger.info(s"[createUserAccount] 执行数据库插入语句：${sql}"))
-      
-      userID <- readDBInt(sql, parameters)
-      _ <- IO(logger.info(s"[createUserAccount] 成功插入用户账号记录，返回的用户 ID: ${userID}"))
-    } yield userID
+      _ <- IO(logger.info(s"[ConstructUserInfo] Constructed UserInfo: ${userInfo}"))
+    } yield userInfo
   }
 }
